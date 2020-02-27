9ebb368d08f5f3717f07ab079848e767b05130df
yak.gjen <yak.gjen@gmail.com>
2020-02-27 14:52:51 +0300

    feat: add a detailed compomponent

---
+++ src/app/youtube/pages/detailed-item/detailed-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detailed-item',
+  templateUrl: './detailed-item.component.html',
+  styleUrls: ['./detailed-item.component.scss']
+})
+export class DetailedItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
